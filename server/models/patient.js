3dddfa210e42c30a11bbba073e15df4f35aa564b
prabhaspp <indra.lochans@gmail.com>
2023-04-28 22:58:09 +0530

    adjustment to db

--- server/models/patient.js
+++ server/models/patient.js
@@ -10,7 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      patient.belongsTo(models.Required, {
+        foreignKey: "member",
+      });
     }
   }
   patient.init({
